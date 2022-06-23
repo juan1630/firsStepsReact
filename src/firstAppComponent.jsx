dcdc3826a25c1e34200b5528df7acaf222d84494
Juan Patrón <josejuanpatron1630@gmail.com>
2022-06-22 23:18:57 -0500

    Fragment en la sintaxis de react

--- src/firstAppComponent.jsx
+++ src/firstAppComponent.jsx
@@ -1,3 +1,11 @@
 export  const FirstApp = () => {
-    return ( <h1> Hola mundo desde react </h1> );
+    return (
+         //No es conveniente que se creen un div para agrupar los elmentos del div, porque puede romper el layout de la aplicaicon
+        
+         //Se recomienda usar esta sintaxis del elemento
+        <>  
+            <h1> Hola mundo desde react </h1> 
+            <p> Soy un parrafo </p>
+        </>
+    );
 }
