fb9983dfc889c79256aa1addd6d7d44bde54c0a2
Juan Patrón <josejuanpatron1630@gmail.com>
2022-06-24 00:34:58 -0500

    Se agregan estilos css y se agrega el pintar variables en el html

--- src/firstAppComponent.jsx
+++ src/firstAppComponent.jsx
@@ -1,10 +1,29 @@
+//const newMessage = "Juan";
+// es mas optimo de esta forma
+// no imprime valores booleanos
+
+//const newMessage = [1,2,3,4,5,6,7];
+//No se puedo imprimir un objeto
+// No usar funciones asincronas para no generar error en el react, ya que es un objeto una promesa
+const saludar = ( ) => 'Hola desde la funcion'
+
+const newMessage = {
+    mensaje: 'Hola',
+    title: "Adios jeje"
+}
+//Para renderizar el objeto se hace con el stringify 
+
+
 export  const FirstApp = () => {
+
     return (
          //No es conveniente que se creen un div para agrupar los elmentos del div, porque puede romper el layout de la aplicaicon
         
-         //Se recomienda usar esta sintaxis del elemento
+         //Se recomienda usar el fragment para no hacer mas complejo el elemento
         <>  
-            <h1> Hola mundo desde react </h1> 
+            <h1> Hola mundo </h1>
+            <p> { saludar() } </p>
+            <code> { JSON.stringify( newMessage ) } </code> 
             <p> Soy un parrafo </p>
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import  ReactDOM  from 'react-dom/client';
 import App  from './HelloWorldApp';
 import {FirstApp} from './firstAppComponent';
 
+//importamos los estilos globales
+
+import './styles.css';
 ReactDOM.createRoot(  document.getElementById('root') ).render(
     <React.StrictMode>
         <App/>
